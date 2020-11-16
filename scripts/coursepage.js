40df8263d1a813e7a86b2faa9605b9c2a05fa739
Federico <federicolucido@gmail.com>
2020-11-16 22:27:23 +0100

    Files download

--- scripts/coursepage.js
+++ scripts/coursepage.js
@@ -1,1 +1,13 @@
-//TO_DO
+$(document).ready(function() { 
+
+    if (isMaterial()) {
+
+        
+
+    }
+
+});
+
+function isMaterial() {
+    return $("#nav_menu li:nth-child(5).active").length
+}
