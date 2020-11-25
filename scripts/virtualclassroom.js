f09c00e09e9b67cd064b4ec4e173f4273d19207a
Federico <federicolucido@gmail.com>
2020-11-25 23:00:45 +0100

    - Hotkeys working everywhere in the page - Volume scroll wheel support

--- scripts/virtualclassroom.js
+++ scripts/virtualclassroom.js
@@ -130,7 +130,7 @@ function newPlayer() {
 
 	video.outerHTML =	`<video id="videoMP4" class="video-js vjs-theme-forest vjs-big-play-centered vjs-playback-rate"
 							controls preload="auto" width="768" height="432"
-							data-setup='{"controls": true, "autoplay": false, "preload": "auto", "playbackRates": [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3]}'>
+							data-setup='{"controls": true, "autoplay": false, "preload": "auto", "playbackRates": [0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3]}'>
 							<source src= ` + mp4Video + ` + type="video/mp4" />
 							<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 						</video>`
@@ -142,6 +142,9 @@ function newPlayer() {
 			volumeStep: 0.1,
 			seekStep: 10,
 			enableModifiersForNumbers: false,
+			captureDocumentHotkeys: true,
+			enableHoverScroll: true,
+			documentHotkeysFocusElementFilter:  e => e.tagName.toLowerCase() === 'body' ,
 			customKeys: {
 				slower: {
 					key: function(event) {
