0c28c23b50c4a2ab932521f485c261f0964e5ba9
gianluca <graziadei.gianluca@gmail.com>
2021-12-08 02:33:39 +0100

    Chiusura #22

--- themes/dark/js/dark.js
+++ themes/dark/js/dark.js
@@ -1,9 +1,29 @@
-$(function() {
-
-    //Remove underline in dark theme
+(function () {
+    //Use loader during page style modification
+    //setLoader();
+    //Prevent overload of Portale della Didattica server
     darkUnderline();
+})();
+
+function setLoader() {
+    var body = document.getElementsByTagName('body')[0];
+    var loader = document.createElement("div");
+    loader.setAttribute("id", "loading_poliTools");
+    var loaderImg = document.createElement("img");
+    loaderImg.setAttribute("id", "loading-image");
+    loaderImg.setAttribute("alt", "Loading");
+    loaderImg.setAttribute("src", "https://didattica.polito.it/img/logo_poli/logo_poli_bianco_260.png");
+    loader.appendChild(loaderImg);
     
-});
+    if(body !== "undefined"){
+        body.appendChild(loader);
+    }
+
+}
+
+function unsetLoader() {
+    $('#loading_poliTools').hide();
+}
 
 function darkUnderline(){
     Array.from(document.getElementsByTagName('span'))
@@ -16,4 +36,8 @@ function darkUnderline(){
                 p.style.color = "white"; 
             }
         });
+}
+
+function setNews() {
+    
 }
