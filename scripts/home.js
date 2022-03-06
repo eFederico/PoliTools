29b068d14770b50ddb2f47b140da52fe3c23143d
e-caste-mbp <git@caste.dev>
2022-03-06 15:55:27 +0100

    replace Telegram groups button with skeeep button on homepage

--- scripts/home.js
+++ scripts/home.js
@@ -2,9 +2,9 @@ $(function() {
 
     $("body").addClass("casa"); 
 
-    let groups = `<a id="div_colsx" class="col-sm-12 col-md-12 portlet_colsx" href="https://t.me/PoliToGruppiBot" target="_blank" style="margin-top: 10px; content:url(chrome-extension://`+exID+`/immagini/groups-button.png)"></a>`;
-
-    $(".sidebar > div:nth-child(1)").after(groups); // Append telegram groups button
+    // let groups = `<a id="div_colsx" class="col-sm-12 col-md-12 portlet_colsx" href="https://t.me/PoliToGruppiBot" target="_blank" style="margin-top: 10px; content:url(chrome-extension://`+exID+`/immagini/groups-button.png)"></a>`;
+    let skeeep = `<a id="div_colsx" class="col-sm-12 col-md-12 portlet_colsx" href="https://weee.link/skeeep" target="_blank" style="margin-top: 10px; content:url(chrome-extension://`+exID+`/immagini/skeeep-button.png)"></a>`;
+    $(".sidebar > div:nth-child(1)").after(skeeep); // Append skeeep button
 
     let matr = $("#div_anagrafica .nav li strong u").html(); // Recupero matricola
 
