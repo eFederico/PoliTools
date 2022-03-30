461cfc8ac947ac160b47e323b4c401a4e567e2d3
alecello <alessandro.celoria@hotmail.com>
2022-03-30 16:47:08 +0200

    Refactor message handler for download status

--- scripts/coursepage.js
+++ scripts/coursepage.js
@@ -1,70 +1,49 @@
-// If running in Firefox, set up the handlers that the background script will use to communicate to the page the
-// download status of the current download.
-if(typeof chrome != "undefined" && typeof browser != "undefined") {
-    let port = browser.runtime.connect({name: "firefoxland"});
-
-    port.onMessage.addListener(function(msg)
-    {
-        if(msg.type === "downloadstatus")
-        {
-            if(msg.val != 0 && msg.val != 4) {
-                document.getElementById("dynamic-container")
-                    .setAttribute('style', 'margin-bottom: 0');
-                document.getElementById("button_download_selected")
-                    .setAttribute('style', 'margin-bottom: 0; display: none');
-            } else {
-                document.getElementById("dynamic-container")
-                    .setAttribute('style', 'margin-bottom: 0; display: none');
-                document.getElementById("button_download_selected")
-                    .setAttribute('style', 'margin-bottom: 0');
-            }
-        }
-        else if(msg.type === "updateProgressBar")
-        {
-            var dynamic = document.getElementById("dynamic");
-
-            dynamic.setAttribute('style', 'width: ' + msg.cs + '%');
-            dynamic.setAttribute('aria-valuenow', msg.cs);
-            dynamic.innerText = msg.string;
-
-            switch(type) {
-                case 0:
-                    dynamic.classList.remove("progress-bar-danger");
-                    dynamic.classList.remove("progress-bar-info");
-                    dynamic.classList.remove("progress-bar-warning");
-                    dynamic.classList.add("progress-bar-success");
-                    break;
-                case 1:
-                    dynamic.classList.remove("progress-bar-danger");
-                    dynamic.classList.remove("progress-bar-success");
-                    dynamic.classList.remove("progress-bar-warning");
-                    dynamic.classList.add("progress-bar-info");
-                    break;
-                case 2:
-                    dynamic.classList.remove("progress-bar-success");
-                    dynamic.classList.remove("progress-bar-info");
-                    dynamic.classList.remove("progress-bar-warning");
-                    dynamic.classList.add("progress-bar-danger");
-                    break;
-                case 3:
-                    dynamic.classList.remove("progress-bar-success");
-                    dynamic.classList.remove("progress-bar-info");
-                    dynamic.classList.remove("progress-bar-danger");
-                    dynamic.classList.add("progress-bar-warning");
-                    break;
-            }
-        }
-    });
+function isNotices() {
+    return $("#nav_menu li:nth-child(3).active").length
 }
 
-$(function() {
-    
-});
+function isGuide() {
+    return $("#nav_menu li:nth-child(4).active").length
+}
 
 function isMaterial() {
     return $("#nav_menu li:nth-child(5).active").length
 }
 
-function isNotices() {
-    return $("#nav_menu li:nth-child(1).active").length
+function isForum() {
+    return $("#nav_menu li:nth-child(6).active").length
+}
+
+function isAssignments() {
+    return $("#nav_menu li:nth-child(7).active").length
+}
+
+function isVideo() {
+    return $("#nav_menu li:nth-child(8).active").length
+}
+
+// If we are running Firefox and we are on the Materials page...
+if(typeof chrome != "undefined" && typeof browser != "undefined" && isMaterial()) {
+    // Connect to the browser.
+    let port = browser.runtime.connect({name: "firefoxland"});
+
+    // Handle download status messages.
+    port.onMessage.addListener(function(msg)
+    {
+        let container = document.getElementById("dynamic-container");
+        let button    = document.getElementById("button_download_selected");
+        
+        if(msg.type === "downloadstatus") {
+            container.style.display = (msg.val != 0 && msg.val != 4) ? 'block' : 'none';
+            button.style.display    = (msg.val != 0 && msg.val != 4) ? 'none'  : 'inline-block';
+        } else if(msg.type === "updateProgressBar") {
+            let dynamic = document.getElementById("dynamic");
+            let statuses = ['success', 'info', 'danger', 'warning'];
+
+            dynamic.setAttribute('aria-valuenow', msg.cs);
+            dynamic.style.width = msg.cs + '%';
+            dynamic.innerText   = msg.string;
+            dynamic.className.replace("progress-bar-[a-z]+", "progress-bar-" + statuses[type]);
+        }
+    });
 }
