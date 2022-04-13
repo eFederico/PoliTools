52f3d5dca677b7639ecfdec6b6dd84a90f7354a4
alecello <alessandro.celoria@hotmail.com>
2022-04-13 16:06:26 +0200

    First rewrite for downloads

--- scripts/virtualclassroom.js
+++ scripts/virtualclassroom.js
@@ -1,23 +1,26 @@
-let urlList = [];
-let lessonlist = [];
-let preurl;
-let seqVideoId;
-let navbarAll = [];
+// List of lessons divided by section.
+let lessonList = [];
 
-$(function() {
+$(async function() {
+    var videos;
 
-    urlList = [];
-    seqVideoId = 0;
-    preurl = "https://didattica.polito.it/portal/pls/portal/";
+    while(true) {
+        videos = document.querySelectorAll('[id^="videoPlayer_"]');
+        if(videos.length > 0) {
+            break;
+        } else {
+            await sleep(100);
+        }
+    }
 
-    if (isConverted(document)) {
-        newPlayer();
-        hotkeysLabels();
-    } else
-        console.log("NOT CONVERTED");
+    for(let index = 0; index < videos.length; ++index) {
+        newPlayer(videos[index]);
+    }
 
+    displayHotkeysLabels();
+
+    // Mark this as a virtual classroom page.
     let HTMLbody = $("body");
-
     HTMLbody.addClass("virtual-classroom");
 
     //TODO Modal ancora utile?
@@ -42,214 +45,78 @@ $(function() {
         $('body').removeClass('modal-active');
     });
 
-    navbarAll = document.querySelectorAll('[id^="lessonList_"]');
-    // questo server per le diverse sezioni di VirtualClassroom
+    // Get ALL the VCs lists across the various panels.
+    // (This year, Past year, Other courses...)
+    sections = document.querySelectorAll('[id^="lessonList_"]');
 
-    for (let i = 0; i < navbarAll.length; i++) {
-        navbar = navbarAll[i]
-        let jdown = document.createElement("button");
-        jdown.className = "btn btn-primary download-all";
-        jdown.innerHTML = "Export JDownloader List";
+    for (let index = 0; index < sections.length; ++index) {
+        // For each panel create a JDownloader button.
+        let jDownloader       = document.createElement("button");
+        jDownloader.className = "btn btn-primary download-all";
+        jDownloader.innerHTML = "Export JDownloader List";
 
-        let downAll = document.createElement("button");
-        downAll.className = "btn btn-primary download-all";
-        downAll.innerHTML = "Download ALL";
+        // For each panel create a Download All button.
+        let downloadAll       = document.createElement("button");
+        downloadAll.className = "btn btn-primary download-all";
+        downloadAll.innerHTML = "Download ALL";
 
-        downAll.addEventListener("click", function() {
-            if (confirm("Sei sicuro di voler scaricare tutte (" + lessonlist[i].length + ") le virtual classroom già convertite?\nL'operazione può richiedere tempo e non può essere annullata.")) {
-                for (let j = 0; j < lessonlist[i].length; j++) {
-                    let d = document.getElementById("directdwn_" + i + "_" + j)
-					console.log(j+ " downloaded")
-					d.click();
+        downloadAll.addEventListener("click", function() {
+            if (confirm("Sei sicuro di voler scaricare tutte (" + lessonList[index].length + ") le virtual classroom già convertite?\nL'operazione può richiedere tempo e non può essere annullata.")) {
+                for (let i = 0; i < lessonList[index].length; ++i) {
+                    downloadLesson(index, i);
                 }
             }
         }, false);
 
-        jdown.addEventListener("click", function() {
-            let list = urlList[i].join("\n");
-            copyToClipboard(list);
+        jDownloader.addEventListener("click", function() {
+            copyToClipboard(lessons[index].map(lesson => lesson.url).join("\n"));
             alert("Link copiati negli appunti! ATTENZIONE! Le lezioni non convertite non verranno aggiunte alla lista\r\nLinks copied to clipboard! Not converted lessons will not be added to the list");
-        }, false)
+        }, false);
 
-        navbar.insertBefore(downAll, navbar.firstChild);
-        navbar.insertBefore(jdown, navbar.firstChild);
+        sections[index].insertBefore(downloadAll, sections[index].firstChild);
+        sections[index].insertBefore(jDownloader, sections[index].firstChild);
+        lessonList[index] = [];
 
-        lessonlist.push(navbar.getElementsByClassName("h5"));
-		urlList.push([]);
-
-        populateDownloadButton(i);
-    }
-
-});
-
-function populateDownloadButton(index) {
-	j = index;
-    if (lessonlist[j]) {
-        for (let i = 0; i < lessonlist[j].length; i++) {
-            let li = lessonlist[j][i];
-            let a = li.getElementsByTagName("a")[0];
-
-            $(li).on("click", "a", function() {
-                console.log("clicked!");
-                newPlayer(i);
+        let lessons = sections[index].getElementsByClassName("h5");
+        for(let i = 0; i < lessons.length; ++i) {
+            // Obtain VC data to later perform the download
+            let link        = lessons[i].getElementsByTagName("a")[0];
+            let bbbid       = link.getAttribute("data-bbb-id");
+            let incarico    = link.getAttribute("data-id_inc");
+            let provvisorio = link.getAttribute("data-id_inc-prov");
+            
+            // Save this to a global storage
+            lessonList[index].push({
+                bbbid,
+                incarico,
+                provvisorio,
             });
 
-            let btn = document.createElement("button");
-            btn.className = "btn btn-primary dwlbtn";
-            btn.id = "directdwn_" + j + "_" + i;
-            btn.innerHTML = '<span class="fa fa-download"></span> Download';
-            btn.ass = a;
-            bbbid = a.getAttribute("data-bbb-id");
-            p_id_inc = a.getAttribute("data-id_inc");
-            p_id_inc_prov = a.getAttribute("data-id_inc-prov");
+            // Create the download button
+            let lessonDownload = document.createElement("button");
+            lessonDownload.className = "btn btn-primary dwlbtn";
+            lessonDownload.id = "directdwn_" + index + "_" + i;
+            lessonDownload.innerHTML = '<span class="fa fa-download"></span> Download';
 
-            $.ajax({
-                data: {
-                    "p_bbbid": bbbid,
-                    "p_id_inc": p_id_inc,
-                    "p_id_inc_prov": p_id_inc_prov,
-                },
-                type: "POST",
-                url: "https://didattica.polito.it/pls/portal30/sviluppo.virtual_classroom_dev.getVCTpl",
-				index: j,
-                success: function(r) {
-                    el = document.createElement('html');
-                    el.innerHTML = r;
+            lessonDownload.addEventListener("click", function() {
+                console.log("Porco dio: " + lessonList[list][index].bbbid);
+                console.log("Downloading lesson (" + index + "," + i + ")");
+                downloadLesson(index, i);
+            }, false);
 
-                    let url = el.querySelector("source").src;
-                    // get url with ajax
-                    let filename = el.querySelector("h3").innerHTML + ".mp4";
-
-                    urlList[this.index].push(url);
-
-                    btn.addEventListener("click", function() {
-
-                        downloadFile(url, filename);
-
-                    }, false);
-
-                    li.insertBefore(btn, li.firstChild);
-                    var res = r
-                    //$("#videoPlayerDiv_245793").html(r);
-                }
-            });
+            lessons[i].insertBefore(lessonDownload, lessons[i].firstChild);
         }
     }
 
-}
+});
 
 function sleep(ms) {
     return new Promise(resolve=>setTimeout(resolve, ms));
 }
 
-async function newPlayer() {
-    await sleep(300);
-
-    let video = $("video")[0];
-    let source = video.src;
-    if (source == null || source == "") {
-        let src = video.children[0];
-        source = src.src;
-    }
-
-    let videoId = "videoMP4_" + seqVideoId++;
-
-    video.outerHTML = `<video id="` + videoId + `" class="video-js vjs-theme-forest vjs-big-play-centered vjs-playback-rate"
-							controls preload="auto" width="768" height="432"
-							data-setup='{"controls": true, "autoplay": false, "preload": "auto", "playbackRates": [0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3]}'>
-							<source src= ` + source + ` + type="video/mp4" />
-							<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
-						</video>`
-
-    let myVideo = videojs(videoId);
-
-    myVideo.ready(function() {
-
-        this.hotkeys({
-            volumeStep: 0.1,
-            seekStep: 10,
-            enableModifiersForNumbers: false,
-            captureDocumentHotkeys: true,
-            enableHoverScroll: true,
-            documentHotkeysFocusElementFilter: e=>e.tagName.toLowerCase() === 'body',
-            customKeys: {
-                //TODO sostituire event.which con nuova implementazione
-                slower: {
-                    key: function(event) {
-                        return (event.which === 74);
-                        // J
-                    },
-                    handler: function() {
-                        let curr = myVideo.playbackRate();
-                        if (curr > 1)
-                            myVideo.playbackRate((curr - 0.1).toFixed(1));
-                    }
-                },
-                faster: {
-                    key: function(event) {
-                        return (event.which === 75);
-                        // K
-                    },
-                    handler: function() {
-                        let curr = myVideo.playbackRate();
-                        if (curr < 3)
-                            myVideo.playbackRate((curr + 0.1).toFixed(1));
-                    }
-                },
-                reset: {
-                    key: function(event) {
-                        return (event.which === 76);
-                        // L
-                    },
-                    handler: function() {
-                        myVideo.playbackRate(1);
-                    }
-                }
-            }
-        });
-    });
-}
-
-function hotkeysLabels() {
-
-    let labels = `<div class = labels>
-					<h3 style="font-size: 21px; margin-top: 21px;" class="cb-title">Hotkeys</h3>
-					<p class="inline"><span class="keyboard-char">J</span> Slower</p>
-					<p class="inline"><span class="keyboard-char">K</span> Faster</p>
-					<p class="inline"><span class="keyboard-char">L</span> Reset</p>
-					<br><br>
-				  </div>`;
-
-    $(".video-js-box").append(labels);
-}
-
+// Check if the video has already been converted (not a WIP BBB recording).
 function isConverted(doc) {
-
     return doc.querySelectorAll('[id^="videoPlayer_"]');
-
-}
-
-function populateList() {
-
-    if (lessonlist == null)
-        return;
-
-    console.log(lessonlist);
-
-    /*let populate = 0;
-    let id;
-
-    if (urlList === "")
-        populate = 1;
-    id = "directdwn_";
-
-    if (populate) {
-        for (let i = 0; i <= lessonlist.length; i++) {
-            let btn = document.getElementById(id + i);
-            retrieveLink(btn);
-        }
-    }*/
-
 }
 
 function downloadFile(url, filename) {
@@ -265,4 +132,93 @@ function downloadFile(url, filename) {
 
 function copyToClipboard(content) {
     navigator.clipboard.writeText(content);
+}
+
+function downloadLesson(list, index) {
+    $.ajax({
+        data: {
+            "p_bbbid"      : lessonList[list][index].bbbid,
+            "p_id_inc"     : lessonList[list][index].incarico,
+            "p_id_inc_prov": lessonList[list][index].provvisorio
+        },
+        type: "POST",
+        url: "https://didattica.polito.it/pls/portal30/sviluppo.virtual_classroom_dev.getVCTpl",
+        index: list,
+        success: function(response) {
+            let url   = response.match(/http(s)?:\/\/video.polito.it\/dl\/[^"]+/)[0];
+            let title = response.match(/<h3>.+<\/h3>/)[0].replace("<h3>", "").replace("</h3>", "");
+
+            console.log("Downloading lesson \"" + title + "\" from " + url);
+            downloadFile(url, title);
+        },
+        error: function(xhr, status, error) {
+            alert("Error downloading media.");
+            console.log("[ERROR] Failed to download VL. Status code: " + xhr.status + " (" + xhr.statusText + ")");
+        }
+    });
+}
+
+function newPlayer(video) {
+    let source  = (video.src == null || video.src == "") ? video.children[0].src : video.src;
+    let videoId = video.id;
+
+    video.outerHTML =  `<video id="` + videoId + `" class="video-js vjs-theme-forest vjs-big-play-centered vjs-playback-rate"
+							controls preload="auto" width="768" height="432"
+							data-setup='{"controls": true, "autoplay": false, "preload": "auto", "playbackRates": [0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3]}'>
+							<source src= ` + source + ` + type="video/mp4" />
+							<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+					    </video>`
+
+    let myVideo = videojs(videoId);
+    myVideo.ready(function() {
+        this.hotkeys({
+            volumeStep: 0.1,
+            seekStep: 10,
+            enableModifiersForNumbers: false,
+            captureDocumentHotkeys: true,
+            enableHoverScroll: true,
+            documentHotkeysFocusElementFilter: e=>e.tagName.toLowerCase() === 'body',
+            customKeys: {
+                //TODO sostituire event.which con nuova implementazione
+                slower: {
+                    key: function(event) {
+                        return (event.which === 74); // J
+                    },
+                    handler: function() {
+                        let curr = myVideo.playbackRate();
+                        if (curr > 1)
+                            myVideo.playbackRate((curr - 0.1).toFixed(1));
+                    }
+                },
+                faster: {
+                    key: function(event) {
+                        return (event.which === 75); // K
+                    },
+                    handler: function() {
+                        let curr = myVideo.playbackRate();
+                        if (curr < 3)
+                            myVideo.playbackRate((curr + 0.1).toFixed(1));
+                    }
+                },
+                reset: {
+                    key: function(event) {
+                        return (event.which === 76); // L
+                    },
+                    handler: function() {
+                        myVideo.playbackRate(1);
+                    }
+                }
+            }
+        });
+    });
+}
+
+function displayHotkeysLabels() {
+    $(".video-js-box").append(`<div class = labels>
+					<h3 style="font-size: 21px; margin-top: 21px;" class="cb-title">Hotkeys</h3>
+					<p class="inline"><span class="keyboard-char">J</span> Slower</p>
+					<p class="inline"><span class="keyboard-char">K</span> Faster</p>
+					<p class="inline"><span class="keyboard-char">L</span> Reset</p>
+					<br><br>
+				  </div>`);
 }
