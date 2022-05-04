4f5811b448a37a0b95a5ec723c2db9ea95dd98db
alecello <alessandro.celoria@hotmail.com>
2022-05-04 11:18:45 +0200

    Reorganized common code and added some facilities:
    - Sleep
    - Perform AJAX request
    - Download file

--- scripts/common.js
+++ scripts/common.js
@@ -1,17 +1,7 @@
 let PoliToolsPort  = chrome.runtime.connect({name: "politools"});
-let PoliToolsDebug = false;
+let PoliToolsDebug = true;
 
-// Allow content scripts to console.log to the extension's console.
-function extensionLog(msg) {
-    if(PoliToolsDebug) {
-        PoliToolsPort.postMessage({
-            msg: "background-log",
-            str: msg
-        });
-    }
-}
-
-// Listen for messages coming from the page.
+// Listen and dispatch messages coming from external (not part of the extension) scripts.
 window.addEventListener("message", function(event) {
     // Only allow messages if they are from the same window and contain both destination and data fields.
     if(event.source != window || !event.data.dst || !event.data.dat)
@@ -19,11 +9,11 @@ window.addEventListener("message", function(event) {
 
     switch(event.data.dst) {
         case "background-script":
-            PoliToolsPort.postMessage(event.data.dat);
+            PoliToolsPort.postMessage(event.data.data);
             break;
         case "background-log":
-            if(event.data.str)
-                extensionLog(event.data.str);
+            if(event.data.data)
+                extensionLog(event.data.data);
             else
                 extensionLog("Received malformed message from page.");
             break;
@@ -32,7 +22,7 @@ window.addEventListener("message", function(event) {
     }
 });
 
-// Listen for messages coming from the background script
+// Listen and dispatch messages coming from the extension's background script.
 PoliToolsPort.onMessage.addListener(function(msg) {
     if(!msg.dst) {
         extensionLog("Received message from background script but no destination was specified.");
@@ -47,3 +37,48 @@ PoliToolsPort.onMessage.addListener(function(msg) {
             extensionLog("Received message from background script but no destination matches '" + msg.dst + "'.");
     }
 });
+
+
+
+// Wait for a certain amount of time to pass.
+// Can be used both with await and .then()
+function sleep(ms) {
+    return new Promise(resolve=>setTimeout(resolve, ms));
+}
+
+// Log a message to the extension's log console
+function extensionLog(msg) {
+    if(PoliToolsDebug) {
+        PoliToolsPort.postMessage({
+            msg: "background-log",
+            data: msg
+        });
+    }
+}
+
+
+
+
+// Perform an Ajax request with the specified parameters:
+//   - data: What has to be sent.
+//   - type: "POST" or "GET" etc.
+//   - url : The URL to contact. 
+function performRequest(data, type, url) {
+    return new Promise(function(resolve, reject) {
+        $.ajax({data, type, url}).done(resolve).fail(reject);
+    });
+}
+
+// Download an arbitrary file with the given filename.
+function downloadFile(url, filename) {
+    extensionLog("Downloading of the following file has been requested and is being dispathed:");
+    extensionLog({url, filename});
+    PoliToolsPort.postMessage({
+        msg: "download-file",
+        data: {
+            subject: "URL",
+            content: url,
+            filename: filename
+        }
+    });
+}
